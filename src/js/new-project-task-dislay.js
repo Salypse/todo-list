5f3f9f1502ccb3af2df110f4fa2f64a185e73df3
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-17 11:40:26 -0500

    Create new project task display and add styles

--- src/js/new-project-task-dislay.js
+++ src/js/new-project-task-dislay.js
@@ -6,9 +6,9 @@ export const newProjectTaskDisplay = function() {
     const newProjectTaskDisplay = document.createElement("div")
     newProjectTaskDisplay.classList.add("new-project-task")
 
-    const newProjectTaskitem = document.createElement("div")
-    newProjectTaskitem.classList.add("new-task-item")
-    newProjectTaskDisplay.append(newProjectTaskitem)
+    const newProjectTaskItem = document.createElement("div")
+    newProjectTaskItem.classList.add("new-task-item")
+    newProjectTaskDisplay.append(newProjectTaskItem)
     
     //New Project Task Header
     const newProjectTaskHeader = document.createElement("div")
@@ -24,7 +24,65 @@ export const newProjectTaskDisplay = function() {
     })
     
     newProjectTaskHeader.append(newProjectTaskHeaderText, newProjectTaskHeaderExit)
-    newProjectTaskitem.append(newProjectTaskHeader)
+    newProjectTaskItem.append(newProjectTaskHeader)
+
+    //New Project Task Form
+    const newProjectTaskForm = document.createElement("form")
+    newProjectTaskForm.classList.add("new-task-form")
+
+    //New Task Name input
+    const newTaskName = document.createElement("input")
+    newTaskName.type = "text"
+    newTaskName.name = "newTaskName"
+    newTaskName.placeholder = "Task Name"
+
+    //New Task Descrription input
+    const newTaskDescription = document.createElement("input")
+    newTaskDescription.type = "text"
+    newTaskDescription.name = "newTaskDescription"
+    newTaskDescription.placeholder = "Task Description"
+    
+    //New Task Date input
+    const newTaskDate = document.createElement("input")
+    newTaskDate.type = "text"
+    newTaskDate.name = "newTaskDate"
+    newTaskDate.placeholder = "Due Date: MM/DD/YY"
+
+    //New Task Priority Selection
+    const newTaskPriority = document.createElement("select")
+    newTaskName.name = "newTaskPriority"
+    let priorityOptions = [
+        {value: "default", text: "Choose Task Priority"},
+        {value: "priority1", text: "1"},
+        {value: "priority2", text: "2"},
+        {value: "priority3", text: "3"},
+    ]
+
+    priorityOptions.forEach(option => {
+        const newOption = document.createElement("option")
+        newOption.value = option.value
+        newOption.textContent = option.text
+        newTaskPriority.append(newOption)
+    })
+
+    //new Task Submit Button
+    const newTaskSubmit = document.createElement("button")
+    newTaskSubmit.type = "submit"
+    newTaskSubmit.textContent = "Submit"
+
+    newProjectTaskForm.append(
+        newTaskName,
+        newTaskDescription,
+        newTaskDate,
+        newTaskPriority,
+        newTaskSubmit
+    )
+
+    newProjectTaskItem.append(newProjectTaskForm)
+    newProjectTaskItem.addEventListener("submit", function(event) {
+        event.preventDefault()
+        exitDisplay(newProjectTaskDisplay)
+    })
 
     content.append(newProjectTaskDisplay)
 }
